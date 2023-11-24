eb850899fa86e791c9dd18d3bc009ce689b22225
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-24 20:11:58 +0700

    first commit

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
-from .models import Brand, Category, Product, Stock
+from .models import Category, Product, Attribute, AttributeValue, Inventory, StockControl, Image
 
-admin.site.register(Brand)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Stock)
+# admin.site.register(Category)
+# admin.site.register(Product)
+# admin.site.register(Attribute)
+# admin.site.register(AttributeValue)
+# admin.site.register(Inventory)
+# admin.site.register(StockControl)
+# admin.site.register(Image)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = Image
+
+
+class StockControlInline(admin.TabularInline):
+    model = StockControl
+
+
+class AttributeValueInline(admin.TabularInline):
+    model = AttributeValue
+
+
+@admin.register(Inventory)
+class ProductInventoryAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline, StockControlInline]
+
+
+@admin.register(Attribute)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [AttributeValueInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
--- inventory/models.py
+++ inventory/models.py
@@ -1,40 +1,79 @@
 from django.db import models
 
 
-class Brand(models.Model):
-    brand_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    is_active = models.BooleanField()
 
     class Meta:
         verbose_name_plural = "Categories"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Product(models.Model):
-    the_name = models.CharField(
-        "Product Name",
-        max_length=100,
-        default="no-name")
-    age = models.IntegerField()
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    slug = models.SlugField()
+    is_active = models.BooleanField()
+
     category = models.ManyToManyField(Category)
 
+    def __str__(self) -> str:
+        return self.name
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class AttributeValue(models.Model):
+    value = models.CharField(max_length=50)
+
+    attribute = models.ForeignKey(
+        Attribute, related_name="attribute", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class Inventory(models.Model):
+    is_active = models.BooleanField()
+    is_default = models.BooleanField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    sku = models.CharField(max_length=20, unique=True)
+
+    product = models.ForeignKey(
+        Product, related_name="product", on_delete=models.CASCADE)
+    attribute_value = models.ManyToManyField(AttributeValue)
+
     class Meta:
-        ordering = ["age"]
+        verbose_name_plural = "Inventory"
 
-    def __str__(self):
-        return f"Product name: {self.the_name}"
+    def __str__(self) -> str:
+        return self.product.name
 
 
-class Stock(models.Model):
-    units = models.BigIntegerField()
-    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+class StockControl(models.Model):
+    last_checked = models.DateTimeField(auto_now_add=False, editable=False)
+    units = models.IntegerField(default=0)
+
+    inventory = models.OneToOneField(Inventory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Stock Control"
+
+
+class Image(models.Model):
+    url = models.ImageField(upload_to=None)
+    alternative_text = models.CharField(max_length=50)
+    is_feature = models.BooleanField()
+
+    inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE)
