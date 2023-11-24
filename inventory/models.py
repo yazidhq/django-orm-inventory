3f68fe6e97e58455c0996a618f699ad37bee6124
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-24 11:33:10 +0700

    Model

--- inventory/models.py
+++ inventory/models.py
@@ -1,10 +1,40 @@
 from django.db import models
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    qty = models.IntegerField()
-
-
 class Brand(models.Model):
+    brand_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    the_name = models.CharField(
+        "Product Name",
+        max_length=100,
+        default="no-name")
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ["age"]
+
+    def __str__(self):
+        return f"Product name: {self.the_name}"
+
+
+class Stock(models.Model):
+    units = models.BigIntegerField()
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
