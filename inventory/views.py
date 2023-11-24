46c2884b48cd8240b00692dfa128c277433e9c25
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-24 21:03:07 +0700

    Views

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
-# Create your views here.
+def all_products(request):
+    objs = Product.objects.all()
+    paginator = Paginator(objs, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'grid-output.html', {'page_obj':page_obj})
