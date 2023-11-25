a3e13dc192a2a9d5ae6594d0bf7104b607d8983e
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2023-11-25 12:24:49 +0700

    Read

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ex1/', views.all_products, name="all_products")
+    path('', views.all_products, name="all_products"),
+    path('view/<str:slug>', views.view, name="view"),
+    path('edit/<str:slug>', views.edit, name="edit"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Inventory, StockControl
 from django.core.paginator import Paginator
 
 def all_products(request):
     objs = Product.objects.all()
-    paginator = Paginator(objs, 6)
+    paginator = Paginator(objs, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'grid-output.html', {'page_obj':page_obj})
+
+
+def view(request, slug):
+    single_product = Product.objects.get(slug=slug)
+    inventory_product = Inventory.objects.get(product__slug=slug)
+    stock_product = StockControl.objects.get(inventory__product__slug=slug)
+    data = {
+        'product':single_product, 
+        'inventory':inventory_product,
+        'stock':stock_product,
+    }
+    return render(request, 'view-single.html', data)
+
+
+def edit(request, slug):
+    return render(request, 'edit-item.html')
